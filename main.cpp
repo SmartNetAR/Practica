b26426226729a8ce6acc6e0e92df964e25022246
Leo <leonardo@smartnet.com.ar>
2017-10-12 20:31:00 -0300

    no message

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 //#include <iostream>
 //#include <stdio.h>
 #include <stdlib.h>
-#include "pqConnect.hpp"
-#include "Arguments.hpp"
+#include "files\pqConnect.hpp"
+#include "files\Arguments.hpp"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
@@ -19,15 +19,17 @@ int main(int argc, char** argv) {
 	pqConnect pq( "localhost", "5432", "apaaa","postgres", "1234" ) ;
 	
 	
-	arguments = "table:animals" ; //getenv("QUERY_STRING") ;
+	//arguments = "table:animal" ; //getenv("QUERY_STRING") ;
+	arguments = getenv("QUERY_STRING") ;
 	Arguments argumentos( arguments ) ;
-	printf( "Argumentos: %s\n\n", argumentos.GetSTR() ) ;
+	printf( "Argumentos: %s\n\n\n", argumentos.GetSTR() ) ;
 	printf( "Metodo: %s\n\n", argumentos.GetMethod() ) ;
 	printf( "Valor: %s\n\n", argumentos.GetValue() ) ;
 	//solucionar esta comparacion
-	if (argumentos.GetMethod() == "table") {
+	pq.Show( argumentos.GetValue() ) ;
+	/*if (argumentos.GetMethod() == "table") {
 		pq.Show( argumentos.GetValue() ) ;
-	}
+	}*/
 	
 	pq.Disconnect() ;
 	printf("<body>\n") ;
